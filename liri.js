230700c7efde2f66541453ee0cca1f79ee614942
Daniel <Danielbornstein98@gmail.com>
2016-06-10 10:54:21 -0400

    another commit

--- liri.js
+++ liri.js
@@ -32,4 +32,47 @@ var getMeSpotify = function(songName){
 	    }
 	});
 }
+var getMyTweets = function(){
+	 
+	var client = new Twitter(keys.twitterKeys);
+	 
+	var params = {screen_name: 'Dan56781'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+	  if (!error) {
+	   
+	  	for(var i=0; i < tweets.length; i++){
+	  		console.log(tweets[i].created_at);
+	  		console.log('');
+	  		console.log(tweets[i].text);
+	  	}
+	  }
+	});	
+}
+
+var getMeMovie = function(movieName){
+
+	if (movieName === undefined){
+		movieName = 'Mr Nobody';
+	}
+
+	var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&r=json";
+
+	request(urlHit, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	var jsonData = JSON.parse(body);
+
+	    console.log('Title: ' + jsonData.Title);
+	    console.log('Year: ' + jsonData.Year);
+	    console.log('Rated: ' + jsonData.Rated);
+	    console.log('IMDB Rating: ' + jsonData.imdbRating);
+	    console.log('Country: ' + jsonData.Country);
+	    console.log('Language: ' + jsonData.Language);
+	    console.log('Plot: ' + jsonData.Plot);
+	    console.log('Actors: ' + jsonData.Actors);
+	   	console.log('Rotten Tomatoes Rating: ' + jsonData.tomatoRating);
+	    console.log('Rotton Tomatoes URL: ' + jsonData.tomatoURL);
+	  }
+	});
+
+}
 
